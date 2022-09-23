3c45213ed2dba32cc4d1afa9f1e2f571c0173196
Alexis1595 <113454850+Alexis1595@users.noreply.github.com>
2022-09-23 14:32:29 +0200

    Create whileIf.js

---
+++ whileIf.js
@@ -0,0 +1,20 @@
+paquete com. bootcamp;
+
+clase pública Mientras {
+    public static void main(String[] args) {
+
+
+        recuento int  = 0;
+        while(contar <=10){
+            contar++;
+            if(recuento == 6)
+                descanso; //Rompe el flujo cuando llega al 6
+                continuar; //Salta el valor 6 y continua a la siguiente interaccion
+
+            Sistema. fuera. println("Hola mundo"+ contar);
+
+
+        }
+        Sistema. fuera. println("fin");
+    }
+}
