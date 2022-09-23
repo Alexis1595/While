7debf083f87477c4defff11a3584ca5c752d7f60
Alexis1595 <113454850+Alexis1595@users.noreply.github.com>
2022-09-23 12:43:50 +0200

    Create while.js

---
+++ while.js
@@ -0,0 +1,15 @@
+package com.bootcamp;
+
+public class While {
+    public static void main(String[] args) {
+
+
+        int count = 0;
+        while(count <=10){
+
+            System.out.println("Hola mundo"+ count);
+            count++;
+
+        }
+        System.out.println("fin");
+    }
